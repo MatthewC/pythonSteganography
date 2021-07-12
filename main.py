71442025aa24e1845268ef4f5e21039b4ac56d8a
Matthew <mateo.cor@mateoc.com>
2021-07-12 16:54:30 +0200

    Created basic class outline.

--- main.py
+++ main.py
@@ -2,6 +2,18 @@ import tkinter as tk
 from tkinter import filedialog
 from PIL import Image
 
+#Define class I'll use to store methods related to the steganography
+class imageSteg:
+    def __init__(self):
+        pass
+
+    def implantData(self, message):
+        pass
+
+    def extractData(self, message):
+        pass
+
+
 #Handle opening an image, and passing it to PIL
 root = tk.Tk()
 root.withdraw()
